195148c34a927c527c9e209f4ac5597accbbb5e7
fredericoferreira77 <frederico.25204708360053@faeterj-rio.edu.br>
2026-09-17 15:07:09 -0300

    Adiciona ativiade Data

---
+++ Data/src/main/java/br/nom/belo/marcio/Data.java
@@ -0,0 +1,30 @@
+package br.nom.belo.marcio;
+
+public class Data 
+{
+
+	public Data(int i, int j, int k) 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getDia() 
+	{
+		return 0;
+	}
+
+	public int getMes() 
+	{
+		return 0;
+	}
+
+	public int getAno() 
+	{
+		return 0;
+	}
+
+	public Data adicionarDias(int i) 
+	{
+		return null;
+	}
+}
