002c15ea5740c9b23daeeb2889d0f3af6174b8db
Frederico Ferreira <frederico.25204708360053@faeterj-rio.edu.br>
2026-09-22 20:22:01 -0300

    Adiciona ativiade Escreva 1 a 100 em PHP

---
+++ Escreva1a100/Escreva1a100.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Escrevendo de 1 a 100</title>
+</head>
+<body>
+
+    <h1>Escrevendo de 1 a 100 utilizando PHP</h1>
+
+    <?php
+        $i = 1;
+        
+        while ($1 <= 100) {
+            echo $1 . "<br>";
+            $1++;
+        }
+
+        echo "Atividade concluida com sucesso!";
+        echo "<br>";
+
+        echo "Frederico Rogério Ferreira";
+        echo "<br>";
+    ?>
+
+</body>
+
+</html>
